a74ed275734fc500c49e53f6a1e61950c025a567
Thomas Krahwinkel <tkrahwinkel@uc.cl>
2024-12-11 10:21:11 -0300

    feat: layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Banner />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
