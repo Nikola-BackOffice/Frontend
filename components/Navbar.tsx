223baa46157f138649c8bc8472a600e7e69e38e5
Thomas Krahwinkel <tkrahwinkel@uc.cl>
2024-12-17 16:24:48 -0300

    Navbar, tw-colors, motion

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,79 @@
-import Image from 'next/image';
-import logo from '../assets/logo.png';
-import { ModeToggle } from './mode-toggle';
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from 'next/navigation'
+
+import { ModeToggle } from "./mode-toggle";
+import logo from "@/assets/logo.png";
+import { cn } from "@/utils/cn";
+import { Panel } from "./Panel";
 
 const Navbar = () => {
+  const pathname = usePathname()
+
+  const paths = [
+    { name: "Clientes", href: "/clients" },
+    { name: "Proyectos", href: "/projects" },
+  ];
+
+  const navigation = paths.map((item) => ({
+    ...item,
+    current: item.href === pathname,
+  }));
+
   return (
-    <div className="bg-gray-800 text-white text-center py-4 flex items-center">
-      <Image
-        src={logo}
-        style={{
-          width: 'auto',
-          height: '40px',
-          filter: 'brightness(0) invert(1)',
-        }}
-        alt="Logo"
-        className="ml-4"
-      />
-      <div className="ml-4">
-        <a href="/clients" className="text-white hover:underline mx-2">
-          Clientes
-        </a>
-        <a href="/projects" className="text-white hover:underline mx-2">
-          Proyectos
-        </a>
-        <ModeToggle />
+    <nav className="bg-gray-800/80">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="relative flex h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <Panel navigation={navigation} />
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <Link href="/">
+                <Image
+                  src={logo}
+                  style={{
+                    width: "auto",
+                    height: "40px",
+                    filter: "brightness(0) invert(1)",
+                  }}
+                  alt="Logo"
+                  className="ml-4"
+                />
+              </Link>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4 h-full items-center">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={cn(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-200 hover:bg-gray-700 hover:text-white",
+                      "rounded-md px-3 py-2 font-medium",
+                    )}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <ModeToggle />
+            <p>User</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </nav>
   );
-}
+};
+
+
 
 export default Navbar;
---
+++ components/Panel.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+
+export const Panel = ({navigation}: {
+  navigation: { name: string; href: string; current: boolean }[];
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenChange = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <DropdownMenu onOpenChange={handleOpenChange}>
+      <DropdownMenuTrigger>
+        {open ? (
+          <X className="size-6" />
+        ) : (
+          <Menu className="size-6" />
+        )}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {navigation.map((item) => (
+          <DropdownMenuItem key={item.name}>
+            <Link href={item.href}>{item.name}</Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
