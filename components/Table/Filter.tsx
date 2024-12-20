45c59a2214ba31a672a9c8d9eea2184d3731b9d2
Thomas Krahwinkel <tkrahwinkel@uc.cl>
2024-12-20 13:40:31 -0300

    dialog finish

--- components/Table/Filter.tsx
+++ components/Table/Filter.tsx
@@ -1,12 +1,17 @@
 import { Table } from '@tanstack/react-table';
 import { Button } from '../ui/button';
 import DialogDemo from '../ui/dialog';
+import { InputForm } from '../forms/example';
 import { DialogDemoShadcn } from '../ui/dialog-example-shadcn';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Filter({ table }: { table: Table<any> }) {
   return (
-    <DialogDemo />
+    <DialogDemo trigger={<button className="text-violet11 shadow-blackA4 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded bg-background px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
+      Edit profile
+    </button>} title="Dialog Title" description="Dialog Description">
+      <InputForm />
+    </DialogDemo>
     // <DialogDemoShadcn />
   );
 }
--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,36 +1,49 @@
-import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
+
+import { cn } from '@/utils/cn';
 import { InputForm } from '../forms/example';
 
-const DialogDemo = () => (
+const DialogDemo = ({
+  className,
+  trigger,
+  title,
+  description,
+  close,
+  children,
+}: {
+  className?: string;
+  trigger: React.ReactNode;
+  title?: string;
+  description?: string;
+  close?: React.ReactNode;
+  children: React.ReactNode;
+}) => (
   <Dialog.Root>
     <Dialog.Trigger asChild>
-      <button className="text-violet11 shadow-blackA4 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded bg-background px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
-        Edit profile
-      </button>
+      {trigger}
     </Dialog.Trigger>
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
 
-      <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
-        <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-          Edit profile
+      <Dialog.Content className={cn("fixed left-1/2 top-1/2 max-h-[85vh] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-md bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg", className)}>
+      <div className='flex flex-col space-y-1.5 text-center sm:text-left'>
+        <Dialog.Title className="text-lg font-semibold leading-none tracking-tight">
+          {title}
         </Dialog.Title>
-        <Dialog.Description className="text-mauve11 mb-5 mt-2.5 text-[15px] leading-normal">
-          Make changes to your profile here. Click save when you're done.
+        <Dialog.Description className="text-sm text-muted-foreground">
+          {description}
         </Dialog.Description>
-        <InputForm />
+        </div>
+        {children}
         <div className="mt-[25px] flex justify-end">
           <Dialog.Close asChild>
-            <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
-              Save changes
-            </button>
+            {close}
           </Dialog.Close>
         </div>
         <Dialog.Close asChild>
           <button
-            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+            className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
             aria-label="Close"
           >
             <X />
